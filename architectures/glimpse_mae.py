Add visualization code, fix attention selector off-by-one error

architectures/glimpse_mae.py
@@ -44,6 +44,8 @@ class BaseGlimpseMae(LightningModule, ABC):
             self.load_pretrained_mae(args.pretrained_mae_path,
                                      segmentation=isinstance(datamodule, BaseSegmentationDataModule))
 
+        self.debug = False
+
     def define_metric(self, name: str, metric_constructor: Callable):
         for mode in ['train', 'val', 'test']:
             setattr(self, f'{mode}_{name}', metric_constructor())
@@ -123,10 +125,14 @@ class BaseGlimpseMae(LightningModule, ABC):
                           device=self.device)
         mask_indices = torch.empty((x.shape[0], 0), dtype=torch.int32, device=self.device)
         loss = 0
+        steps = []
         # zero step (initialize decoder attention weights)
         with torch.no_grad():
             latent = self.mae.forward_encoder(x, mask_indices)
             pred = self.mae.forward_decoder(latent, mask, mask_indices)
+            if self.debug:
+                steps.append(
+                    (mask.detach().clone().cpu(), pred.detach().clone().cpu(), None))
         if self.sum_losses:
             losses = []
             for i in range(self.num_glimpses):
@@ -134,19 +140,24 @@ class BaseGlimpseMae(LightningModule, ABC):
                 pred = self.forward_one(x, mask_indices, mask)
                 if compute_loss:
                     losses.append(self.calculate_loss_one(pred, mask, batch))
+                if self.debug:
+                    steps.append(
+                        (mask.detach().clone().cpu(), pred.detach().clone().cpu(), self.glimpse_selector.debug_info))
             if compute_loss:
                 loss = self.calculate_loss(losses, batch)
-            return {"out": pred, "mask": mask, "losses": losses, "loss": loss}
+            return {"out": pred, "mask": mask, "losses": losses, "loss": loss, "steps": steps}
         else:
             with torch.no_grad():
                 for i in range(self.num_glimpses - 1):
                     mask, mask_indices = self.glimpse_selector(mask, mask_indices, i)
-                    self.forward_one(x, mask_indices, mask)
+                    pred = self.forward_one(x, mask_indices, mask)
+                    if self.debug:
+                        steps.append((mask.detach().clone().cpu(), pred.detach().clone().cpu()))
             mask, mask_indices = self.glimpse_selector(mask, mask_indices, i)
             pred = self.forward_one(x, mask_indices, mask)
             if compute_loss:
                 loss = self.calculate_loss_one(pred, mask, batch).mean()
-            return {"out": pred, "mask": mask, "losses": [loss], "loss": loss}
+            return {"out": pred, "mask": mask, "losses": [loss], "loss": loss, "steps": steps}
 
     def do_metrics(self, mode, out, batch):
         pass
@@ -168,6 +179,9 @@ class BaseGlimpseMae(LightningModule, ABC):
         out = self.forward(batch)
         self.log_metric('test', 'loss', out['loss'])
         self.do_metrics('test', out, batch)
+
+    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> Any:
+        return self.forward(batch, compute_loss=False)
 
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay, betas=(0.9, 0.95))

architectures/selectors.py
@@ -12,6 +12,7 @@ class BaseGlimpseSelector:
         self.glimpse_size = args.glimpse_size
         self.grid_h = model.mae.grid_size[0]
         self.grid_w = model.mae.grid_size[1]
+        self.debug_info = None
 
     @classmethod
     def add_argparse_args(cls, parent_parser):
@@ -24,7 +25,17 @@ class BaseGlimpseSelector:
         return parent_parser
 
     def __call__(self, *args, **kwargs):
+        self.debug_info = {}
         return self.forward(*args, **kwargs)
+
+    def _add_glimpses(self, glimpses, mask, mask_indices):
+        glimpses = glimpses.repeat(1, self.glimpse_size)
+        for idx in range(1, self.glimpse_size):
+            glimpses[:, idx] += idx
+        glimpses = torch.cat([glimpses + self.grid_w * idx for idx in range(self.glimpse_size)], dim=1)
+        mask = mask.scatter(1, glimpses, torch.full_like(mask, fill_value=True))
+        mask_indices = torch.cat((mask_indices, glimpses), dim=1)
+        return mask, mask_indices
 
     @abc.abstractmethod
     def forward(self, current_mask, mask_indices, glimpse_num):
@@ -35,17 +46,11 @@ class RandomGlimpseSelector(BaseGlimpseSelector):
 
     def forward(self, mask, mask_indices, glimpse_num):
         """True in mask represents patches kept, False removed"""
-        N, L = mask.shape
+        N = mask.shape[0]
         new_glimpse_x = torch.randint(0, self.grid_w - 2, size=(N, 1), device=mask.device)
         new_glimpse_y = torch.randint(0, self.grid_h - 2, size=(N, 1), device=mask.device)
         glimpses = self.grid_w * new_glimpse_y + new_glimpse_x
-        glimpses = glimpses.repeat(1, self.glimpse_size)
-        for idx in range(1, self.glimpse_size):
-            glimpses[:, idx] += idx
-        glimpses = torch.cat([glimpses + self.grid_w * idx for idx in range(self.glimpse_size)], dim=1)
-        mask = mask.scatter(1, glimpses, torch.full_like(mask, fill_value=True))
-        mask_indices = torch.cat((mask_indices, glimpses), dim=1)
-        return mask, mask_indices
+        return self._add_glimpses(glimpses, mask, mask_indices)
 
 
 class CheckerboardGlimpseSelector(BaseGlimpseSelector):
@@ -55,19 +60,12 @@ class CheckerboardGlimpseSelector(BaseGlimpseSelector):
                             (1, 5), (5, 5), (9, 5), (13, 5)]
 
     def forward(self, mask, mask_indices, glimpse_num):
-        N, L = mask.shape
+        N = mask.shape[0]
         to_take = self.coordinates[glimpse_num]
         new_glimpse_x = torch.full((N, 1), fill_value=to_take[0], device=mask.device)
         new_glimpse_y = torch.full((N, 1), fill_value=to_take[1], device=mask.device)
         glimpses = self.grid_w * new_glimpse_y + new_glimpse_x
-        glimpses = glimpses.repeat(1, self.glimpse_size)
-        for idx in range(1, self.glimpse_size):
-            glimpses[:, idx] += idx
-        glimpses = torch.cat([glimpses + self.grid_w * idx for idx in range(self.glimpse_size)], dim=1)
-        mask.scatter_(1, glimpses, torch.full_like(mask, fill_value=True))
-        mask_indices = torch.cat((mask_indices, glimpses), dim=1)
-
-        return mask, mask_indices
+        return self._add_glimpses(glimpses, mask, mask_indices)
 
 
 class AttentionGlimpseSelector(BaseGlimpseSelector):
@@ -88,13 +86,12 @@ class AttentionGlimpseSelector(BaseGlimpseSelector):
 
     def forward(self, current_mask, mask_indices, glimpse_num):
         with torch.no_grad():
-            current_mask = ~current_mask
             # get self attention weights
             attn = self.model.mae.last_attn[self.attention_layer][..., 1:, 1:]
             B = attn.shape[0]
 
             # set attention weights to known patches to 0
-            attn = attn * current_mask.reshape((B, 1, -1, 1))
+            attn = attn * (~current_mask).reshape((B, 1, -1, 1))
 
             # calculate entropy of attention weights for each output patch
             entropy = (-attn * torch.log2(attn))
@@ -105,33 +102,24 @@ class AttentionGlimpseSelector(BaseGlimpseSelector):
             # calculate sampling weights for next glimpse
 
             entropy = entropy.reshape(shape=(B, 1, self.grid_h, self.grid_w))
-            next_mask = nn.functional.avg_pool2d(entropy, self.glimpse_size, 1, (self.glimpse_size - 1) // 2)
+            next_mask = nn.functional.avg_pool2d(entropy, kernel_size=self.glimpse_size, stride=1, padding=0)
             next_mask = nn.functional.pad(next_mask,
                                           (0, self.grid_w - next_mask.shape[3], 0, self.grid_h - next_mask.shape[2]),
                                           mode='constant', value=0)
             assert next_mask.shape == (B, 1, self.grid_h, self.grid_w)
+            if self.model.debug:
+                self.debug_info['entropy'] = entropy.detach().clone()
             del entropy
 
-            next_mask[:, :, [0, -1]] = -1e8
-            next_mask[:, :, :, [0, -1]] = -1e8
+            if self.model.debug:
+                selection_map = next_mask.detach().clone()
+                s = selection_map.shape
+                selection_map = selection_map.reshape(B, -1)
+                selection_map = nn.functional.softmax(selection_map, dim=1)
+                selection_map = selection_map.reshape(s)
+                self.debug_info['selection_map'] = selection_map
+
             next_mask = next_mask.reshape(B, -1)
             # select next glimpse
-            arg = torch.argmax(next_mask, dim=1).unsqueeze(1)
-            next_mask[:] = 0
-            next_mask.scatter_(1, arg, next_mask + 1)
-            next_mask = next_mask.reshape(B, 1, self.grid_h, self.grid_w)
-            next_mask = nn.functional.max_pool2d(next_mask, self.glimpse_size, 1, (self.glimpse_size - 1) // 2)
-            next_mask = nn.functional.pad(next_mask,
-                                          (0, self.grid_w - next_mask.shape[3], 0, self.grid_h - next_mask.shape[2]),
-                                          mode='constant', value=0)
-            assert next_mask.shape == (B, 1, self.grid_h, self.grid_w)
-            next_mask = next_mask <= 0
-            next_mask = next_mask.reshape(B, -1)
-
-            # Mask to indices
-            cumsum = torch.cumsum(torch.ones_like(next_mask), dim=1) - 1
-            new_indices = cumsum[~next_mask].reshape(B, -1)
-            mask_indices = torch.cat((mask_indices, new_indices), dim=1)
-            # add next glimpse to current mask
-            next_mask = next_mask * current_mask
-        return ~next_mask, mask_indices
+            glimpses = torch.argmax(next_mask, dim=1).unsqueeze(1)
+        return self._add_glimpses(glimpses, current_mask, mask_indices)

train.py
@@ -1,50 +1,25 @@
 import argparse
-import inspect
 import platform
 import random
 import sys
 import time
 
-import pytorch_lightning
 import torch
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, RichProgressBar, RichModelSummary
 from pytorch_lightning.loggers import WandbLogger, TensorBoardLogger
 from pytorch_lightning.plugins import NativeMixedPrecisionPlugin
 from pytorch_lightning.strategies import DDPStrategy
 from torch.cuda.amp import GradScaler
 
-import architectures
-import datasets
+from utils.prepare import experiment_from_args
 
 random.seed(1)
 torch.manual_seed(1)
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog='TrainWhereToLookNext'
-    )
-
-    archs = {k: v for k, v in inspect.getmembers(architectures, inspect.isclass) if
-             issubclass(v, pytorch_lightning.LightningModule) and not inspect.isabstract(v)}
-    dss = {k: v for k, v in inspect.getmembers(datasets, inspect.isclass) if
-           issubclass(v, pytorch_lightning.LightningDataModule) and not inspect.isabstract(
-               v) and k != 'LightningDataModule'}
-
-    parser.add_argument('dataset', help='Dataset to use', choices=dss.keys())
-    parser.add_argument('arch', help='Model architecture', choices=archs.keys())
-    main_args = parser.parse_args(sys.argv[1:3])
-
-    arch_class = archs[main_args.arch]
-    data_module_class = dss[main_args.dataset]
-
-    parser = argparse.ArgumentParser(
-        prog=f'TrainWhereToLookNext {main_args.dataset} {main_args.arch}',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser = arch_class.add_argparse_args(parser)
-    parser = data_module_class.add_argparse_args(parser)
+def define_args(parent_parser):
+    parser = parent_parser.add_argument_group('train.py')
     parser.add_argument('--epochs',
                         help='number of epochs',
                         type=int,
@@ -72,16 +47,11 @@ def main():
                         type=bool,
                         default=False,
                         action=argparse.BooleanOptionalAction)
+    return parent_parser
 
-    args = parser.parse_args(sys.argv[3:])
-    setattr(args, 'dataset', main_args.dataset)
-    setattr(args, 'arch', main_args.arch)
 
-    data_module = data_module_class(args)
-    model = arch_class(args, data_module)
-
-    if args.load_model_path:
-        model.load_from_checkpoint(args.load_model_path)
+def main():
+    data_module, model, args = experiment_from_args(sys.argv, add_argparse_args_fn=define_args)
 
     print(
         f'The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters\n')
@@ -105,10 +75,10 @@ def main():
     checkpoint_callback = ModelCheckpoint(dirpath=f"checkpoints/{run_name}", monitor="val/loss")
 
     trainer = Trainer(plugins=plugins, max_epochs=args.epochs, accelerator='auto', logger=loggers,
-                      callbacks=[checkpoint_callback],
+                      callbacks=[checkpoint_callback, RichProgressBar(leave=True), RichModelSummary(max_depth=3)],
                       strategy=DDPStrategy(find_unused_parameters=False) if args.ddp else None)
 
-    trainer.fit(model=model, datamodule=data_module)
+    trainer.fit(model=model, datamodule=data_module, ckpt_path=args.load_model_path)
 
     if data_module.has_test_data:
         trainer.test(ckpt_path='best', datamodule=data_module)

utils/visualize.py
@@ -1,42 +1,69 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from PIL import Image
 
-from data_utils.datasets import IMAGENET_MEAN, IMAGENET_STD
+from datasets.utils import IMAGENET_MEAN, IMAGENET_STD
 
 _imagenet_mean = torch.tensor(IMAGENET_MEAN).unsqueeze(1).unsqueeze(1)
 _imagenet_std = torch.tensor(IMAGENET_STD).unsqueeze(1).unsqueeze(1)
 
 
-def save_reconstructions(mae, output, batch, visualizations_to_save, dump_path):
+def save_reconstructions(model, output, batch, vis_id, dump_path):
+    mae = model.mae
     with torch.no_grad():
-        bsz = output["out"].shape[0]
+        B = batch[0].shape[0]
+        steps = output['steps']
+        reconstructed = torch.stack([x[1] for x in steps], dim=1).reshape(
+            B * len(steps), *output['out'].shape[1:])
+        mask = torch.stack([x[0] for x in steps], dim=1)
+        has_att_info = len(steps[1][2]) > 0
+        if has_att_info:
+            entropies = torch.stack([x[2]['entropy'].cpu() for x in steps[1:]], dim=1)
+            selection_maps = torch.stack([x[2]['selection_map'].cpu() for x in steps[1:]], dim=1)
+
         reconstructed = torch.clip(
-            (mae.unpatchify(output["out"]).detach().cpu() * _imagenet_std + _imagenet_mean) * 255, 0, 255)
+            (mae.unpatchify(reconstructed) * _imagenet_std + _imagenet_mean) * 255, 0, 255)
+        reconstructed = reconstructed.reshape(B, len(steps), *reconstructed.shape[1:])
+
         target = torch.clip((batch[0].cpu() * _imagenet_std + _imagenet_mean) * 255, 0, 255)
 
-        # Paste ground truth glimpses into reconstructed image
-        reconstructed_pasted = reconstructed.clone()
-        a = mae.patchify(reconstructed_pasted)
-        a[output["mask"], :] = mae.patchify(target)[output["mask"].cpu(), :]
-        reconstructed_pasted = mae.unpatchify(a)
+        grid_h = mae.grid_size[0]
+        grid_w = mae.grid_size[1]
+        msk = mask.reshape(B, len(steps), grid_h, grid_w)
 
         # Dump visualizations
-        if visualizations_to_save > 0:
-            os.makedirs(dump_path, exist_ok=True)
-            for i in range(bsz):
-                out_image = np.array(reconstructed[i].permute(1, 2, 0), dtype=np.uint8)
-                out_image = Image.fromarray(out_image)
-                out_image.save(f"{dump_path}/{visualizations_to_save}_reconstructed.png")
-                out_image = np.array(reconstructed_pasted[i].permute(1, 2, 0), dtype=np.uint8)
-                out_image = Image.fromarray(out_image)
-                out_image.save(f"{dump_path}/{visualizations_to_save}_reconstructed_pasted.png")
-                trg_image = np.array(target[i].permute(1, 2, 0), dtype=np.uint8)
-                trg_image = Image.fromarray(trg_image)
-                trg_image.save(f"{dump_path}/{visualizations_to_save}_target.png")
+        os.makedirs(dump_path, exist_ok=True)
+        for i in range(B):
+            path = f"{dump_path}/{vis_id}_{i}.jpg"
+            fig, axs = plt.subplots(5 if has_att_info else 3, 9)
+            for s in range(len(steps)):
+                fig.set_size_inches(20, 7 if has_att_info else 5)
+                fig.tight_layout()
 
-                visualizations_to_save -= 1
-                if visualizations_to_save <= 0:
-                    break
+                trg_image = np.array(target[i].permute(1, 2, 0), dtype=np.uint8)
+                axs[0, s].imshow(trg_image)
+                axs[0, s].set_title('input')
+
+                msk_image = np.array(msk[i][s])
+                axs[1, s].imshow(msk_image)
+                axs[1, s].set_title('mask')
+
+                out_image = np.array(reconstructed[i][s].permute(1, 2, 0), dtype=np.uint8)
+                axs[2, s].imshow(out_image)
+                axs[2, s].set_title('out')
+
+                if has_att_info:
+                    if s < entropies.shape[1]:
+                        axs[3, s].imshow(entropies[i][s][0])
+                        axs[3, s].set_title('entropies')
+
+                        axs[4, s].imshow(selection_maps[i][s][0])
+                        axs[4, s].set_title('next glimpse')
+                    else:
+                        axs[3, s].axis('off')
+                        axs[4, s].axis('off')
+
+            fig.savefig(path)
+            plt.cla()